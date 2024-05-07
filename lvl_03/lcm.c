6879688c229bb30ed038b886d8bb2cbad238f761
zayoussef <136023225+zayoussef@users.noreply.github.com>
2024-05-07 16:38:38 +0100

    Update lcm.c

--- lvl_03/lcm.c
+++ lvl_03/lcm.c
@@ -9,7 +9,7 @@ unsigned int lcm(unsigned int a, unsigned int b)
         n = b;
     while (1)
     {
-        if(n % a == 0 || n % b == 0)
+        if(n % a == 0 && n % b == 0)
             return n;
         n++;
     }
